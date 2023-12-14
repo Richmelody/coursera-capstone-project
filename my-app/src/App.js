0c8f0ec71b605757a846e732a7023d4e1ccb8d6b
richmelody <chigs1million@gmail.com>
2023-12-14 22:38:44 +0100

    working on my nav menu.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,19 @@
 import "./App.css";
 import React from "react";
 import Header from "./Header";
-// import Nav from "./Nav";
+import Nav from "./Nav";
 import Main from "./Main";
 import Footer from "./Footer";
 
-
-
-
 function App() {
-  return <>
-  <Header />
-  {/* <Nav />  */}
-  <Main />  
-  <Footer />
-  ;
-</>
-  }
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />;
+    </>
+  );
+}
 
 export default App;
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Header.css"; // Import the CSS file
-import Nav from "./Nav"; // Import the Nav component
 import logo from "./assets/Rectangle 7.png"; // Import the logo image
 
 const Header = () => {
@@ -9,7 +8,6 @@ const Header = () => {
       <a href="/" className="header-logo">
         <img src={logo} alt="little lemon" className="header-image-left" />
       </a>
-      <Nav />
     </header>
   );
 };
