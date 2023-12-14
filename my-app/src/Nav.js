5faceb09c5f9402f985f7106ef275f6171ddab6a
Richmelody <chigs1million@gmail.com>
2023-12-14 17:46:44 +0100

    working on my responsiveness

--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './Nav.css'; // Import the CSS file
+import React, { useState } from 'react';
+import './Nav.css'; // Replace with your actual CSS file path
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navigation">
-      <ul>
+      <ul className={`navigation-list ${isOpen ? 'visible' : ''}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#menu">Menu</a></li>
@@ -12,6 +16,9 @@ const Nav = () => {
         <li><a href="#order-online">Order Online</a></li>
         <li><a href="#login">Login</a></li>
       </ul>
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        {/* Add hamburger menu icon or text */}
+      </button>
     </nav>
   );
 };
